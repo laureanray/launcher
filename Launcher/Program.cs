09220d81089bead77b7a33904dc2e404cae2f7d7
unknown <laureanraybahala@gmail.com>
2021-02-19 12:44:03 +0800

    initial commit

---
+++ Launcher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Launcher
+{
+    using System;
+    using System.IO;
+    using System.Diagnostics;
+    using System.Reflection;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                if (args.Length != 0)
+                {
+                    string executable = args[2];
+                    /*uncomment the below three lines if the exe file is in the Assets  
+                     folder of the project and not installed with the system*/
+                    //string path=Assembly.GetExecutingAssembly().CodeBase;
+                    // string directory=Path.GetDirectoryName(path);
+                    // Process.Start(directory+"\\"+executable);
+                    Process.Start(executable);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
